9e50f1632157c69dd3bce815c68200c80d7b5748
Jenni <justlittleoldmejenn@gmail.com>
2020-10-24 17:11:52 -0700

    Starting Project Pages
    Login, Home, Unauthorized Access, Blackmail entry pages.

---
+++ Pages/Unauthorized.razor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Radzen;
+using Radzen.Blazor;
+
+namespace Blackmail.Pages
+{
+    public partial class UnauthorizedComponent
+    {
+
+    }
+}
---
+++ Pages/Unauthorized.razor.designer.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.JSInterop;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Radzen;
+using Radzen.Blazor;
+using Blackmail.Models.BlackmailDb;
+using Microsoft.AspNetCore.Identity;
+using Blackmail.Models;
+using Blackmail.Client.Pages;
+
+namespace Blackmail.Pages
+{
+    public partial class UnauthorizedComponent : ComponentBase
+    {
+        [Parameter(CaptureUnmatchedValues = true)]
+        public IReadOnlyDictionary<string, dynamic> Attributes { get; set; }
+
+        public void Reload()
+        {
+            InvokeAsync(StateHasChanged);
+        }
+
+        public void OnPropertyChanged(PropertyChangedEventArgs args)
+        {
+        }
+
+        [Inject]
+        protected IJSRuntime JSRuntime { get; set; }
+
+        [Inject]
+        protected NavigationManager UriHelper { get; set; }
+
+        [Inject]
+        protected DialogService DialogService { get; set; }
+
+        [Inject]
+        protected TooltipService TooltipService { get; set; }
+
+        [Inject]
+        protected ContextMenuService ContextMenuService { get; set; }
+
+        [Inject]
+        protected NotificationService NotificationService { get; set; }
+
+        [Inject]
+        protected SecurityService Security { get; set; }
+
+
+        [Inject]
+        protected BlackmailDbService BlackmailDb { get; set; }
+
+        protected async System.Threading.Tasks.Task LoginButtonClick(MouseEventArgs args)
+        {
+            UriHelper.NavigateTo("login");
+        }
+    }
+}
