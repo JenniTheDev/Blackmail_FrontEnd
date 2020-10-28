24227be33d21089a97adfd2ad0ed9af2700b8f40
Jenni <justlittleoldmejenn@gmail.com>
2020-10-28 15:50:56 -0700

    Pausing Project and switching approaches

--- server/Models/blackmailazure/Datum.cs
+++ server/Models/blackmailazure/Datum.cs
@@ -4,46 +4,36 @@ using System.Text.Json.Serialization;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace Blackmail.Models.Blackmailazure {
-    [Table("data")]
-    public partial class Datum {
-        [Key]
-        public int UserID {
-            get;
-            set;
-        }
-
-        public string UserName {
-            get;
-            set;
-        }
-        public string Email {
-            get;
-            set;
-        }
-        public string Password {
-            get;
-            set;
-        }
-        public bool Admin {
-            get;
-            set;
-        }
-        public string Blackmailer {
-            get;
-            set;
-        }
-        public string Blackmailee {
-            get;
-            set;
-        }
-        public double? cost {
-            get;
-            set;
-        }
-        public string img {
-            get;
-            set;
-        }
+namespace Blackmail.Models.Blackmailazure
+{
+  [Table("data")]
+  public partial class Datum
+  {
+    [Key]
+    public int UserID
+    {
+      get;
+      set;
     }
+    public string Blackmailer
+    {
+      get;
+      set;
+    }
+    public string Blackmailee
+    {
+      get;
+      set;
+    }
+    public double? cost
+    {
+      get;
+      set;
+    }
+    public string img
+    {
+      get;
+      set;
+    }
+  }
 }
